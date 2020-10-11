ab973902cd4a7ab4a8ca3cc00a262ca00d104453
PeiyuLu <luxx5940@mylaurier.ca>
2020-10-11 01:03:50 -0400

    Finished part 3

--- app/src/main/java/com/code/wlu/peiyu/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/code/wlu/peiyu/androidassignments/ListItemsActivity.java
@@ -1,14 +1,129 @@
 package com.code.wlu.peiyu.androidassignments;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ListItemsActivity extends AppCompatActivity {
+
+    protected static final String ACTIVITY_NAME="ListItemsActivity";
+    private Object View;
+    static final int REQUEST_IMAGE_CAPTURE = 10;
+    AppCompatActivity anActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        //click_image(findViewById(R.id.imageButton));
+
+    }
+
+    public void click_image(View view){
+        //ImageButton imgButton = findViewById(R.id.imageButton);
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePicture.resolveActivity(getPackageManager())!= null){
+            startActivityForResult(takePicture,REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE &&
+                resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageButton imageButton = findViewById(R.id.imageButton);
+            imageButton.setImageBitmap(imageBitmap);
+        }
+
+
+    }
+
+    public boolean switch_status = false;
+    public void setOnCheckedChanged(View view){
+        CharSequence text;
+        int duration;
+        if (switch_status == false) {
+            //text = setTitle(R.string.text_on);//"Switch is On";// "Switch is Off"
+            duration = Toast.LENGTH_SHORT; //= Toast.LENGTH_LONG if Off
+            switch_status = true;
+            Toast toast = Toast.makeText(this , R.string.text_on, duration); //this is the ListActivity
+            toast.show(); //display your message box
+        }
+        else{
+            //text = "Switch is off";
+            duration = Toast.LENGTH_LONG;
+            switch_status = false;
+            Toast toast = Toast.makeText(this , R.string.text_off, duration); //this is the ListActivity
+            toast.show(); //display your message box
+        }
+    }
+    public void OnCheckChanged(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+// 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+
+                .setTitle(R.string.dialog_title)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        Intent resultIntent = new Intent(  );
+                        resultIntent.putExtra("Response", "Here is my response");
+                        setResult(Activity.RESULT_OK, resultIntent);
+                        finish();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                })
+                .show();
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
--- app/src/main/java/com/code/wlu/peiyu/androidassignments/MainActivity.java
+++ app/src/main/java/com/code/wlu/peiyu/androidassignments/MainActivity.java
@@ -2,13 +2,87 @@ package com.code.wlu.peiyu.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static final String ACTIVITY_NAME="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListItemsActivity.class);
+                startActivityForResult(intent, 10);
+
+            }
+        });
+
+    }
+//    public void startAnotherActivity(View v){
+//        Intent sIntent = new Intent(MainActivity.this,
+//                ListItemsActivity.class);
+//        startActivity(sIntent);
+//
+//    }
+
+    public void onActivityResult(int requestCode, int responseCode, Intent data) {
+
+        super.onActivityResult(requestCode, responseCode, data);
+        if (requestCode == 10){
+            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+        }
+        if (responseCode == Activity.RESULT_OK){
+            String messagePassed = data.getStringExtra("Response");
+            //CharSequence text = "ListItemsActivity passed: My information to share";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(this , R.string.passActivity, duration); //this is the ListActivity
+            toast.show(); //display your message box
+
+        }else{
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
